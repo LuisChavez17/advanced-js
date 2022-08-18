eedf66b7a3506471a892adac1a8157db51855627
Luis Chavez <goldenchavez17@gmail.com>
2022-08-18 16:01:58 -0500

    Asincronia es cuando en un progama de codigo se ejecutan primero las lineas de codigo que no dependen de factor externo como una API, SQL, etc

--- 3-Asincronia/3.1-sincrono-asincrono.js
+++ 3-Asincronia/3.1-sincrono-asincrono.js
@@ -1,12 +1,18 @@
 /**
+ * BLOQUE 1
  * Código sincrono: Una linea se jecuta inmediatamente despues de la otra
  * no hay codigo dependiente o bloqueante.
  */
 
-console.log('============== BLOQUE 1 ==============');
-console.log('Primero'); 
-console.log('Segundo');
-console.log('Tercero');
+// console.log('============== BLOQUE 1 ==============');
+// console.log('Primero'); 
+// console.log('Segundo');
+// console.log('Tercero');
+
+/**
+ * BLOQUE 2 - Problema tipico de asincronia
+ * 
+ */
 
 const validatingPayment = () => {
     setTimeout(() => {
@@ -17,6 +23,5 @@ const validatingPayment = () => {
 console.log('1 Presionar el boton comprar');
 validatingPayment();
 console.log('3 Ver el mensaje de gracias por su compra joven');
-
 
 
