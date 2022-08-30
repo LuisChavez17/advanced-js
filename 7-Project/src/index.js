e88e09928a8690a8b84be4dddc33543e9bf6ac22
Luis Chavez <goldenchavez17@gmail.com>
2022-08-29 22:45:45 -0500

    feat: file index.js was modified to add import APIroutes from routes

--- 7-Project/src/index.js
+++ 7-Project/src/index.js
@@ -7,6 +7,8 @@ const app = express();
 
 app.set('port', process.env.PORT || 4000);
 
+// import APIroutes from "./routes/product.routes";
+
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
@@ -16,3 +18,4 @@ app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')} is listening`.rainbow);
 });
 
+// app.use("/api-restfull/meals",APIroutes);
