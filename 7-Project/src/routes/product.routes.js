65a09fae6346b343316d4c1e2b4bb0b2fe75e252
Luis Chavez <goldenchavez17@gmail.com>
2022-08-30 23:48:22 -0500

    feat: file product.routes.js  was modified to create reoutes for API

--- 7-Project/src/routes/product.routes.js
+++ 7-Project/src/routes/product.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { methods as MealsController } from "./../controllers/product.controllers"
+import { methods as DataRestController } from "./../controllers/product.controllers"
 
 const APIroutes = Router();
 
 /* A router that is used to create the routes for the API. */
-APIroutes.get("/", MealsController.getMeals); 
-
+APIroutes.get("/", DataRestController.getDataRest); // GET http://localhost:4000/api-restfull/Restaurant/
+APIroutes.get("/meals", DataRestController.getMeals);
+APIroutes.get("/meals/:id", DataRestController.getMealsbyId);
+APIroutes.post("/meals", DataRestController.addNewMeal);
 
 export default APIroutes;
