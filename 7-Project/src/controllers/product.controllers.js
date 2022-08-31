14a0dcf6e11d036aabc9919fe3bcbe6bf2ccbb18
Luis Chavez <goldenchavez17@gmail.com>
2022-08-30 23:46:30 -0500

    feat: file product.controllers.js  was modified to create methods

--- 7-Project/src/controllers/product.controllers.js
+++ 7-Project/src/controllers/product.controllers.js
@@ -1,10 +1,46 @@
-import {getConnection} from "./../services/Connection"
+import { getConnection } from "./../services/Connection"
 
 
-const getMeals = async (req, res) =>{
-    try{
+const getDataRest = async (req, res) => {
+    try {
         const connection = await getConnection();
         const data = await connection;
+        res.json(data)
+    } catch (error) {
+        res.status(500);
+        res.send(error.message)
+    }
+}
+const getMeals = async (req, res) => {
+    try {
+        const connection = await getConnection();
+        const data = await connection;
+        res.json(data.meals)
+    } catch (error) {
+        res.status(500);
+        res.send(error.message)
+    }
+}
+const getMealsbyId = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const connection = await getConnection();
+        const data = await connection.meals.find(((meal => meal.id === parseInt(id))));
+        res.json(data)
+    } catch (error) {
+        res.status(500);
+        res.send(error.message)
+    }
+}
+
+const addNewMeal = async (req, res) =>{
+    try{
+        const { id, name, price, content, categoryid } = req.body;
+        if (id === undefined || name === undefined || price === undefined || content === undefined || categoryid === undefined ){
+            res.status(400).json({message:"Bad Request. Please fill all field."})
+        }
+        const connection = await getConnection();
+        const data = await connection.meals.push(((newMeal =>(`id`, `name`, `price`, `content`, `categoryid`) === "?,?,?,?,?," [id, name, price, content, categoryid])));
         res.json(data)
     }catch (error){
         res.status(500);
@@ -12,10 +48,13 @@ const getMeals = async (req, res) =>{
     }
 }
 
+
+
 export const methods = {
+    getDataRest,
     getMeals,
-    // getMeal,
-    // addMeal,
+    getMealsbyId,
+    addNewMeal
     // updateMeal,
     // deleteMeal
 
