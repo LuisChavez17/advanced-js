83871ce21767c2d4613aed411dcb908a4081074d
Luis Chavez <goldenchavez17@gmail.com>
2022-08-30 23:44:17 -0500

    feat: file Connection.js  was modified on sintaxis to make tests

--- 7-Project/src/services/Connection.js
+++ 7-Project/src/services/Connection.js
@@ -1,6 +1,6 @@
-import meals from "./db.json";
+import data from "./db.json";
 
-const connection = meals;
+const connection = data;
 
 const getConnection = () =>{
     return connection
@@ -9,3 +9,31 @@ const getConnection = () =>{
 module.exports = {
     getConnection
 }
+
+
+
+
+
+// import { Low, JSONFile } from "lowdb";
+// import { join, dirname } from "path";
+// import { fileURLToPath } from "url";
+
+// let db;
+
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// export async function createConnection() {
+//   // Use JSON file for storage
+//   const file = join(__dirname, "../db.json");
+//   const adapter = new JSONFile(file);
+//   db = new Low(adapter);
+
+//   // Read data from JSON file, this will set db.data content
+//   await db.read();
+
+//   db.data ||= { tasks: [] };
+//   // Write db.data content to db.json
+//   await db.write();
+// }
+
+// export const getConnection = () => db;
