047eee0f1e096a033eb8f4fdf77439e45f60f27e
Luis Chavez <goldenchavez17@gmail.com>
2022-09-04 16:37:43 -0500

    feat: file product.controllers.js was modified to create method Delete and modified methos that require ID params to evaluate if Id exists

--- 7-Project/src/controllers/product.controllers.js
+++ 7-Project/src/controllers/product.controllers.js
@@ -26,6 +26,10 @@ const getMealsbyId = async (req, res) => {
         const { id } = req.params;
         const connection = await getConnection();
         const data = await connection.meals.find(((meal => meal.id === parseInt(id))));
+        if (!data){
+            res.status(404).json({ message: "Bad Request. ID not founded." });
+            return;
+        }
         res.json(data)
     } catch (error) {
         res.status(500);
@@ -58,7 +62,10 @@ const updateMeal = async (req, res) => {
         }
         const connection = await getConnection();
         const data = await connection.meals.find(((meal => meal.id === parseInt(id))));
-        if (data) {
+        if (!data){
+            res.status(404).json({ message: "Bad Request. ID not founded." });
+            return;
+        } else if (data) {
             data.name = name;
             data.price = price;
             data.content = content;
@@ -81,7 +88,10 @@ const modifieMeal = async (req, res) => {
         }
         const connection = await getConnection();
         const data = await connection.meals.find(((meal => meal.id === parseInt(id))));
-        if ((name) && (data.name = name)) {
+        if (!data){
+            res.status(404).json({ message: "Bad Request. ID not founded." });
+            return;
+        } else if ((name) && (data.name = name)) {
             res.json(data)
         } else if ((price) && (data.price = price)) {
             res.json(data)
@@ -97,23 +107,55 @@ const modifieMeal = async (req, res) => {
     }
 }
 
+const deleteMeal = async (req, res) =>{
+    try{
+        const {id} = req.params;
+        const connection = await getConnection();
+        const data = await connection.meals.find(((meal => meal.id === parseInt(id))));
+        if (!data){
+            res.status(404).json({ message: "Bad Request. ID not founded." });
+            return;
+        }   
+        // const updateMeals = connection.meals.splice(data, 1);
+        const updateMeals = await connection.meals.filter(((meal => meal.id !== parseInt(id))));
+        connection.meals = updateMeals;
+        res.json(updateMeals)
+    }catch (error){
+        res.status(500);
+        res.send(error.message)
+    }
+}
 
+// const indexFounded = this.shoes.findIndex(shoe => shoe.id === parseInt(id));
+//                 if (indexFounded !== -1) {
+//                     const shoesCopy = [ ...this.shoes ];
+//                     shoesCopy.splice(indexFounded, 1);
+//                     this.shoes = [ ...shoesCopy ];
+//                     resolve();
+//                 }
 
-
-
-// try {
+// xport const deleteTask = async (req, res) => {
 //     const db = getConnection();
 //     const taskFound = db.data.tasks.find((t) => t.id === req.params.id);
-//     if (!taskFound) return res.sendStatus(404);
-
-//     taskFound.name = name;
-//     taskFound.description = description;
-
-//     db.data.tasks.map((t) => (t.id === req.params.id ? taskFound : t));
-
+//     if (!taskFound) res.sendStatus(404);
+  
+//     const newTasks = db.data.tasks.filter((t) => t.id !== req.params.id);
+//     db.data.tasks = newTasks;
 //     await db.write();
+  
+//     return res.json(taskFound);
+//   };
 
-//     res.json(taskFound);
+// router.delete('/:id', (req, res) => {
+//     const {id} = req.params;
+//     if (id) {
+//         _.each(movies, (movie, i) => {
+//             if (movie.id == id) {
+//                 movies.splice(i, 1);
+//             }
+//         });
+//         res.json(movies);
+//     }
 
 
 export const methods = {
@@ -123,7 +165,6 @@ export const methods = {
     addNewMeal,
     updateMeal,
     modifieMeal,
-    // deleteMeal
-
+    deleteMeal
 }
 
